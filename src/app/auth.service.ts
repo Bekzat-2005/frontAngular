839f30289128089488d7db80d17f2fd2fdd9faf1
Bekzat <baymukach@gmail.com>
2025-04-28 03:12:28 +0500

    Production build updated

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { environment } from '../environments/environment';
+
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   // public api = 'http://localhost:3000';
-  public api = environment.apiUrl;
+  public api = 'https://backexpress-3mnu.onrender.com'; // Міне осылай!
 
 
   constructor(private http: HttpClient) {}
